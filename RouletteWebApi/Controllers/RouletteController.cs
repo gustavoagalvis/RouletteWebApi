186abe5cae0e58e37583ab99bb3e589802b12f46
--unset <gustavoagalvis@gmail.com>
2020-08-03 09:05:08 -0500

    Init(RouletteWebApi):Initiation of the project

---
+++ RouletteWebApi/Controllers/RouletteController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+namespace RouletteWebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RouletteController : ControllerBase
+    {
+    }
+}
